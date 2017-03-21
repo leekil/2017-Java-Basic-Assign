98a8da197cceb7c605e0ef7c5d426dbf7caa35a2
leekil <dlrlf369@naver.com>
2017-03-21 15:50:57 +0900

    Update Average.java

--- Average.java
+++ Average.java
@@ -8,7 +8,7 @@ public class Average { 
         int size = input.nextInt();    
         System.out.println("\n중간에 0을 입력하면 이전 입력자료로 계산하고 종료합니다.");  
         System.out.print("실수 " + size + "개를 입력하세요. >> ");    
-        
+        0[]
         //프로그래밍 하세요.
         
         System.out.println("합: " + sum + ", 평균: " + sum/size);
