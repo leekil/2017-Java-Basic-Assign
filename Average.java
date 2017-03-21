3b4f0da1584b9e3060b2d1ac1e498e3dd04ec812
leekil <dlrlf369@naver.com>
2017-03-21 18:04:19 +0900

    20130668 이길
    과제 제출합니다.

--- Average.java
+++ Average.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Average { 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.print("입력할 최대 실수의 갯수를 입력하세요. >> ");    
         Scanner input = new Scanner(System.in);  
@@ -9,8 +9,16 @@ public class Average { 
         System.out.println("\n중간에 0을 입력하면 이전 입력자료로 계산하고 종료합니다.");  
         System.out.print("실수 " + size + "개를 입력하세요. >> ");    
         
-        //프로그래밍 하세요.
-        
+        for (int j = 0; j < size; j++) {  
+            x[j] = input2.nextDouble();  
+            if (x[j] == 0) {   
+                size = j; // 0입력시 현재까지 입력한 갯수   
+                break;  
+        } 
+        }
+        for (int i = 0; i <= size; i++) {  
+                sum += x[i]; // 입력한 실수의 합                        
+        }
         System.out.println("합: " + sum + ", 평균: " + sum/size);
-    }
+}
 }
